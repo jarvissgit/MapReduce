540d77bff88cfb09ef41b25baea7fb5a7bf8010d
J P <km4gck@gmail.com>
2017-04-02 23:50:37 +0530

    first codes

---
+++ src/summaries/Reduce.java
@@ -0,0 +1,10 @@
+package summaries;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class Reduce {
+}
