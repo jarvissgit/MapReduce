9610b50516f000257a90a4af148294eb37d2a08c
J P <km4gck@gmail.com>
2017-04-03 00:51:19 +0530

    working on the main.java to create job object

--- src/summaries/Main.java
+++ src/summaries/Main.java
@@ -1,7 +1,14 @@
 package summaries;
 
-/**
- * Created by root on 3/4/17.
- */
-public class Main {
-}
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class Main extends Configured implements Tool {}
